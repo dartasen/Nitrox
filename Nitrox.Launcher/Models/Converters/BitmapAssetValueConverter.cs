5ac5b3af2ceb6432f2bcfdcd1328848c2d20f85d
dartasen <cleeeemove@gmail.com>
2023-01-23 23:30:37 +0100

    Added basic playpage design

--- Nitrox.Launcher/Models/Converters/BitmapAssetValueConverter.cs
+++ Nitrox.Launcher/Models/Converters/BitmapAssetValueConverter.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Reflection;
 using Avalonia;
 using Avalonia.Data.Converters;
@@ -9,6 +10,11 @@ using Avalonia.Platform;
 
 namespace Nitrox.Launcher.Models.Converters;
 
+/// <summary>
+/// Converts a string path to a bitmap asset.
+/// The asset must be in the same assembly as the program. If it isn't,
+/// specify "avares://<assemblynamehere>/" in front of the path to the asset.
+/// </summary>
 public class BitmapAssetValueConverter : BaseConverter<BitmapAssetValueConverter>, IValueConverter
 {
     private static readonly string assemblyName = Assembly.GetEntryAssembly()?.GetName().Name ?? throw new Exception("Unable to get Assembly name");
@@ -20,10 +26,12 @@ public class BitmapAssetValueConverter : BaseConverter<BitmapAssetValueConverter
         {
             return null;
         }
+
         if (value is not string rawUri || !targetType.IsAssignableFrom(typeof(Bitmap)))
         {
             throw new NotSupportedException();
         }
+
         if (assetCache.TryGetValue(rawUri, out Bitmap bitmap))
         {
             return bitmap;
@@ -40,15 +48,13 @@ public class BitmapAssetValueConverter : BaseConverter<BitmapAssetValueConverter
             uri = new Uri($"avares://{assemblyName}{rawUri}");
         }
 
-        IAssetLoader assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
-        if (assets != null)
-        {
-            bitmap = new Bitmap(assets.Open(uri));
-            assetCache.Add(rawUri, bitmap);
-            return bitmap;
-        }
+        IAssetLoader assets = AvaloniaLocator.Current.GetRequiredService<IAssetLoader>();
+        Stream asset = assets.Open(uri);
 
-        return null;
+        bitmap = new Bitmap(asset);
+        assetCache.Add(rawUri, bitmap);
+
+        return bitmap;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
