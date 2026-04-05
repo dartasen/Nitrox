666c40360eb43df6210c06db01e54fe1c86b2661
dartasen <10561268+dartasen@users.noreply.github.com>
2026-04-06 01:06:36 +0200

    WIP

--- Nitrox.Launcher/ViewModels/Designer/DesignOptionsViewModel.cs
+++ Nitrox.Launcher/ViewModels/Designer/DesignOptionsViewModel.cs
@@ -7,27 +7,31 @@ internal class DesignOptionsViewModel : OptionsViewModel
 {
     public DesignOptionsViewModel() : base(null!, null!)
     {
-        KnownGame selectedGame = new()
-        {
-            PathToGame = @"C:\Users\Me\Games\Subnautica",
-            Platform = Platform.STEAM,
-            SelectCommand = SelectDetectedGameCommand,
-            IsSelected = true
-        };
-
+        KnownGame selectedGame = CreateKnownGame(@"C:\Steam\steamapps\common\Subnautica", Platform.STEAM, isSelected: true);
         SelectedGame = selectedGame;
+        
         KnownInstallations.Add(selectedGame);
-        KnownInstallations.Add(new KnownGame
-        {
-            PathToGame = @"D:\Games\Epic Games\Subnautica",
-            Platform = Platform.EPIC,
-            SelectCommand = SelectDetectedGameCommand
-        });
+        KnownInstallations.Add(CreateKnownGame(@"D:\Games\Epic Games\Subnautica", Platform.EPIC));
+        KnownInstallations.Add(CreateKnownGame(@"E:\Heroic\Prefixes\Subnautica", Platform.HEROIC));
+        KnownInstallations.Add(CreateKnownGame(@"C:\XboxGames\Subnautica\Content", Platform.MICROSOFT));
+        KnownInstallations.Add(CreateKnownGame(@"C:\Users\Me\AppData\Local\DiscordGames\Subnautica", Platform.DISCORD));
+        KnownInstallations.Add(CreateKnownGame(@"/Applications/Subnautica.app/Contents", Platform.NONE));
 
         LaunchArgs = "-vrmode none";
         ProgramDataFolderDir = @"C:\Users\Me\AppData\Roaming\Nitrox";
         ScreenshotsFolderDir = @"C:\Users\Me\AppData\Roaming\Nitrox\screenshots";
         SavesFolderDir = @"C:\Users\Me\AppData\Roaming\Nitrox\saves";
         LogsFolderDir = @"C:\Users\Me\AppData\Roaming\Nitrox\logs";
+    }
+
+    private static KnownGame CreateKnownGame(string path, Platform platform, bool isSelected = false)
+    {
+        return new KnownGame
+        {
+            PathToGame = path,
+            Platform = platform,
+            SelectCommand = null!,
+            IsSelected = isSelected
+        };
     }
 }
